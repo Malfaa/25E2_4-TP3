bcfc8aabe3b3ba8f3542835c818651ec53d594d9
marufa <gmalfacorrea@gmail.com>
2025-06-05 20:54:17 -0300

    Exercicio9, feito

---
+++ CityBreaks.Web/Pages/CreateProperty.cshtml.cs
@@ -0,0 +1,61 @@
+using CityBreaks.Web.Data;
+using CityBreaks.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityBreaks.Web.Pages;
+
+public class CreateProperty : PageModel
+    {
+        private readonly CityBreaksContext _context; 
+
+        public CreateProperty(CityBreaksContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Property NewProperty { get; set; } = new(); 
+
+        public SelectList CityDropdown { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            await PopulateCityOptionsAsync();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+
+            if (!ModelState.IsValid)
+            {
+                await PopulateCityOptionsAsync();
+                return Page();
+            }
+
+            _context.Properties.Add(NewProperty);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+                return RedirectToPage("/CityDetails", new { name = (await _context.Cities.FindAsync(NewProperty.CityId))?.Name });
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                await PopulateCityOptionsAsync();
+                return Page();
+            }
+        }
+
+        private async Task PopulateCityOptionsAsync()
+        {
+            var cities = await _context.Cities
+                                     .OrderBy(c => c.Name)
+                                     .Select(c => new { c.Id, c.Name })
+                                     .ToListAsync();
+            CityDropdown = new SelectList(cities, nameof(City.Id), nameof(City.Name));
+        }
+    }
